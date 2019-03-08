6adcec46948922c44f3d8a1a92a210e0aaf19bc2
Gunjan Kalita <g.kalita@borgos.in>
2019-03-08 13:38:12 +0530

    All matches route implemented

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,11 @@ import React from 'react'
 import Layout from './Hoc/Layout';
 import { Switch, Route } from 'react-router-dom';
 import Home from './Components/home/index';
-import SingIn from './Components/SignIn/index'; 
+import SingIn from './Components/SignIn/index';
 import Dashboard from './Components/Admin/Dashboard';
 import PrivateRoute from './Components/authRoutes/privateRoutes';
 import PublicRoutes from './Components/authRoutes/publicRoutes';
+import AdminMatches from './Components/Admin/matches/index';
 
 
 
@@ -14,11 +15,12 @@ const Routes = (props) => {
     return (
         <Layout>
             <Switch>
-            <PrivateRoute {...props} path="/dashboard" exact component={Dashboard}/>
-              <PublicRoutes {...props} restricted={false} path="/" exact  component = {Home}  />
-              <PublicRoutes {...props} restricted={true} path="/sing_In" exact  component = {SingIn}  />
+                <PrivateRoute {...props} path="/admin_matches" exact component={AdminMatches} />
+                <PrivateRoute {...props} path="/dashboard" exact component={Dashboard} />
+                <PublicRoutes {...props} restricted={false} path="/" exact component={Home} />
+                <PublicRoutes {...props} restricted={true} path="/sing_In" exact component={SingIn} />
 
-                
+
 
 
             </Switch>
